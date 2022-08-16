6351c8785fe617e4fe57c16d30768e2d7ce016bd
Пользователь <a.l.e.k.s.r.u.m.e.n98@gmail.com>
2022-08-16 17:02:59 +0300

    Updated tests

--- src/components/tests/Cell.test.tsx
+++ src/components/tests/Cell.test.tsx
@@ -1,28 +1,72 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { Provider, useDispatch } from 'react-redux';
+import { createStore } from 'redux';
 import userEvent from '@testing-library/user-event'
 import Cell from '../Cell';
+import reducer from '../../reducer';
+import { setState } from '../../reducer/sudoku';
 
+describe('test component Cell', () => {
+    let store: any
 
+    beforeEach(() => {
+        store = createStore(reducer)
+    })
 
-it('render Cell active', () => {
-    render(<Cell number={5} disabledCells={[]} />);
-    const cellActive = screen.queryByTestId('active')
-    expect(cellActive).toBeInTheDocument();
-})
+    test('render Cell active', () => {
+        render(
+            <Provider store={store}>
+                <Cell number={5} disabledCells={[]} />
+            </Provider>
+        );
+        const cellActive = screen.queryByTestId('active')
+        expect(cellActive).toBeInTheDocument();
+    })
 
-it('render Cell disabled', () => {
-    render(<Cell number={5} disabledCells={[5]} />);
-    const cellDisabled = screen.queryByTestId('disabled')
-    expect(cellDisabled).toBeInTheDocument()
-})
+    test('render Cell disabled', () => {
+        render(
+            <Provider store={store}>
+                <Cell number={5} disabledCells={[5]} />
+            </Provider>
+        );
+        const cellDisabled = screen.queryByTestId('disabled')
+        expect(cellDisabled).toBeInTheDocument()
+    })
 
-it('check status error', () => {
-    render(<Cell number={5} disabledCells={[]} />);
-    const cell = screen.queryByTestId('active')
-    userEvent.type(cell!, '6')
-    expect(document.getElementsByClassName('ant-input-number-status-error').length).toBe(1)
+    test('check status error if hints on', () => {
+        const TestComponent = () => {
+            const dispatch = useDispatch()
+            dispatch(setState({ hints: true }))
 
-    userEvent.type(cell!, '{backspace}5')
-    expect(document.getElementsByClassName('ant-input-number-status-error').length).toBe(0)
+            return (null)
+        }
+
+        render(
+            <Provider store={store}>
+                <TestComponent />
+                <Cell number={5} disabledCells={[]} />
+            </Provider>
+        );
+        const cell = screen.queryByTestId('active')
+        userEvent.type(cell!, '6')
+        expect(document.getElementsByClassName('ant-input-number-status-error').length).toBe(1)
+
+        userEvent.type(cell!, '{backspace}5')
+        expect(document.getElementsByClassName('ant-input-number-status-error').length).toBe(0)
+    })
+
+    test('check status error if hints off', () => {
+        render(
+            <Provider store={store}>
+                <Cell number={5} disabledCells={[]} />
+            </Provider>
+        );
+        const cell = screen.queryByTestId('active')
+        userEvent.type(cell!, '6')
+        expect(document.getElementsByClassName('ant-input-number-status-error').length).toBe(0)
+
+        userEvent.type(cell!, '{backspace}5')
+        expect(document.getElementsByClassName('ant-input-number-status-error').length).toBe(0)
+    })
 })
--- src/components/tests/Square.test.tsx
+++ src/components/tests/Square.test.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import { render, screen } from '@testing-library/react';
 import Square from '../Square';
+import reducer from '../../reducer';
 
 const mockSquare = Array.from(Array(9), (param, index) => index + 1)
 
-it('render Square', () => {
-    render(<Square square={mockSquare} gridTemplate={'auto auto auto'} />)
-    const gridWrap = screen.queryByTestId('gridWrap')
-    expect(gridWrap?.childElementCount).toBe(mockSquare.length)
+describe('test component Cell', () => {
+    let store: any
+
+    beforeEach(() => {
+        store = createStore(reducer)
+    })
+
+    test('render Square', () => {
+        render(
+            <Provider store={store}>
+                <Square square={mockSquare} gridTemplate={'auto auto auto'} />
+            </Provider>
+        )
+        const gridWrap = screen.queryByTestId('gridWrap')
+        expect(gridWrap?.childElementCount).toBe(mockSquare.length)
+    })
 })
